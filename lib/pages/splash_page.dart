6626780ccefabbd93e97538499564e7eca71e76e
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-04-11 22:11:43 +0100

    refactoring

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,4 @@
-import 'package:WaifuHub/widgets/bottomNavBar.dart';
+import 'package:WaifuHub/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
