f6aecfb0b6567e15a0c4b0b7b350587cda9cb46c
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-04-12 16:15:22 +0100

    correctly loads user's profile pic from firestore

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,5 +1,7 @@
+import 'package:WaifuHub/global/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Account extends StatefulWidget {
   Account({Key key}) : super(key: key);
@@ -9,12 +11,42 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseUser user;
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.currentUser().then(setUser).catchError(setError);
+  }
+
+  void setUser(FirebaseUser user) {
+    setState(() {
+      this.user = user;
+      this.error = null;
+    });
+  }
+
+  void setError(e) {
+    setState(() {
+      this.user = null;
+      this.error = e.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        child: Text("Logout"),
-        onPressed: () => _signOut(context),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Indstillinger"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _genProfPic(context),
+          ],
+        ),
       ),
     );
   }
@@ -23,5 +55,42 @@ class _AccountState extends State<Account> {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context)
         .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
+  }
+
+  Widget _fetchUsername(BuildContext context) {
+    return new StreamBuilder(
+        stream: Firestore.instance
+            .collection('users')
+            .document(user.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return new Text("Loading");
+          }
+          var userDocument = snapshot.data;
+          return new Text(
+            userDocument.data['username'].toString(),
+            style: headingLarge,
+          );
+        });
+  }
+
+  Widget _genProfPic(BuildContext context) {
+    return new StreamBuilder(
+        stream: Firestore.instance
+            .collection('users')
+            .document(user.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return new Text("Loading");
+          }
+          var userDocument = snapshot.data;
+          var picUrl = userDocument.data['profPicUrl'].toString();
+         return new CircleAvatar(
+           radius: 100,
+           backgroundImage: NetworkImage(picUrl),
+         );
+        });
   }
 }
