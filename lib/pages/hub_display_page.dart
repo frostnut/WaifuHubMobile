d71a2e162b3d644beb495ca99e42d0000a4eb60b
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-04-26 18:28:45 +0100

    added functions to like, unlike, follow and unfollow

--- lib/pages/hub_display_page.dart
+++ lib/pages/hub_display_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -14,6 +16,30 @@ class HubDisplay extends StatefulWidget {
 }
 
 class _HubDisplayState extends State<HubDisplay> {
+  FirebaseUser _user;
+  String _error;
+
+  /// sets current user
+  void setUser(FirebaseUser user) {
+    setState(() {
+      this._user = user;
+      this._error = null;
+    });
+  }
+
+  /// sets error if error fetching user
+  void setError(e) {
+    setState(() {
+      this._user = null;
+      this._error = e.toString();
+    });
+  }
+
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.currentUser().then(setUser).catchError(setError);
+  }
+
   @override
   Widget build(BuildContext context) {
     // Extract the arguments from the current ModalRoute settings and cast
@@ -70,7 +96,7 @@ class _HubDisplayState extends State<HubDisplay> {
                           icon: Icon(Icons.thumb_up),
                           iconSize: 30.0,
                           color: lightPinkColor,
-                          onPressed: () => Navigator.pop(context),
+                          onPressed: () => _likePage(args.id),
                         ),
                         IconButton(
                           icon: Icon(FontAwesomeIcons.comment),
@@ -145,5 +171,54 @@ class _HubDisplayState extends State<HubDisplay> {
         ],
       ),
     );
+  }
+
+  /// handles logic of liking a page, essentially
+  /// just fetches the current number of likes,
+  /// increments them and then puts new number of
+  /// likes into the db
+  /// also makes user follow the page
+  void _likePage(String hubId) async {
+    DocumentSnapshot snap =
+        await Firestore.instance.collection("hubs").document(hubId).get();
+    int pageLikes = snap.data["likes"];
+    pageLikes += 1;
+    Firestore.instance.collection("hubs").document(hubId).updateData({
+      'likes': pageLikes,
+    });
+    _followPage(hubId);
+  }
+
+  /// adds this hub id to user's hub list
+  void _followPage(String hubId) async {
+    List<dynamic> newHub = [];
+    newHub.add(hubId);
+    Firestore.instance
+        .collection("users")
+        .document(_user.uid)
+        .updateData({"hubIDs": FieldValue.arrayUnion(newHub)});
+  }
+
+  /// handles logic to unlike a page, decrements the 
+  /// likes and unfollows user from the page
+  void _unlikePage(String hubId) async {
+    DocumentSnapshot snap =
+        await Firestore.instance.collection("hubs").document(hubId).get();
+    int pageLikes = snap.data["likes"];
+    pageLikes -= 1;
+    Firestore.instance.collection("hubs").document(hubId).updateData({
+      'likes': pageLikes,
+    });
+    _unFollowPage(hubId);
+  }
+
+  /// removes this hub id to user's hub list
+  void _unFollowPage(String hubId) async {
+    List<dynamic> removeHub = [];
+    removeHub.add(hubId);
+    Firestore.instance
+        .collection("users")
+        .document(_user.uid)
+        .updateData({"hubIDs": FieldValue.arrayRemove(removeHub)});
   }
 }
